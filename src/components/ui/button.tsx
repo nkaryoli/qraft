9f2c85d3a96388f861ff6ec78309c97fc45cb52a
karyoli.ie@gmail.com <148868767+nkaryoli@users.noreply.github.com>
2025-05-10 00:40:01 +0200

    feat: refactor button

--- src/components/ui/button.tsx
+++ src/components/ui/button.tsx
@@ -11,7 +11,7 @@ const buttonVariants = cva(
     {
         variants: {
             variant: {
-                default: 'bg-primary text-primary-foreground shadow-xs hover:bg-primary/90',
+                default: 'bg-primary text-primary-foreground shadow-xs hover:bg-primary/80',
                 destructive:
                     'bg-destructive text-white shadow-xs hover:bg-destructive/90 focus-visible:ring-destructive/20 dark:focus-visible:ring-destructive/40 dark:bg-destructive/60',
                 outline:
@@ -22,8 +22,8 @@ const buttonVariants = cva(
             },
             size: {
                 default: 'h-9 px-4 py-2 has-[>svg]:px-3',
-                sm: 'h-8 rounded-md gap-1.5 px-3 has-[>svg]:px-2.5',
-                lg: 'h-10 rounded-md px-6 has-[>svg]:px-4',
+                sm: 'h-6 rounded-md gap-1.5 px-3 has-[>svg]:px-2.5',
+                lg: 'h-10 rounded-md px-6 has-[>svg]:px-4 text-md',
                 icon: 'size-9',
             },
         },
