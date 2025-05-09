56ed77a6381bc6e872a77ad4cee428a1a68e60f7
karyoli.ie@gmail.com <148868767+nkaryoli@users.noreply.github.com>
2025-05-09 14:42:45 +0200

    feat: add useIsMobile hook

---
+++ src/hooks/useIsMobile.tsx
@@ -0,0 +1,22 @@
+import { useEffect, useState } from "react";
+
+export const useIsMobile = (breakpoint: number = 976) => {
+    const [isMobile, setIsMobile] = useState(window.innerWidth < breakpoint);
+
+    useEffect(() => {
+		let timeoutId: number;
+		
+        const handleResize = () => {
+            clearTimeout(timeoutId);
+            timeoutId = window.setTimeout(() => {
+                setIsMobile(window.innerWidth < breakpoint);
+            }, 100);
+        };
+
+        window.addEventListener("resize", handleResize);
+        
+        return () => window.removeEventListener("resize", handleResize);
+    }, [breakpoint]);
+
+    return isMobile;
+};
