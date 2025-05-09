daa469e8f43e8f8b197bcccd2a3094eabc6bbe34
karyoli.ie@gmail.com <148868767+nkaryoli@users.noreply.github.com>
2025-05-09 09:29:08 +0200

    feat: add background style to layout

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -8,6 +8,7 @@ interface LayoutProps {
 const Layout: React.FC<LayoutProps> = ({ children }) => {
     return (
         <div className="min-h-screen flex flex-col">
+            <div className="grid-background"></div>
             <Navbar />
             <main className="flex-1 flex items-center justify-center">{children}</main>
             <Footer />
