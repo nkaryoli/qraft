a619f5e240a17a05024da24338d919747fe9d9ab
karyoli.ie@gmail.com <148868767+nkaryoli@users.noreply.github.com>
2025-05-10 22:46:28 +0200

    feat: reponsive SignInBtn component

--- src/components/header/SignInButton.tsx
+++ src/components/header/SignInButton.tsx
@@ -1,45 +1,54 @@
 import { SignedIn, SignedOut, UserButton } from "@clerk/clerk-react"
 import { Button } from "../ui/button"
 import { memo, useEffect, useState } from "react";
+import { Heart, ScanQrCode } from "lucide-react";
+import { FaUser } from "react-icons/fa";
 
-const SignInBtn = ({onClick}:{onClick: () => void}) => {
+const SignInBtn = ({ isMobile, onClick }:{ isMobile: boolean, onClick: () => void}) => {
 	const [isClient, setIsClient] = useState(false);
 
 	useEffect(() => {
 		setIsClient(true);
 	}, []);
 
+	const buttonSize = isMobile === true ? "default" : "lg";
+	const buttonLabel = isMobile === true ? <FaUser /> : 'Sign in';
+	
 	return (
 		<div className="ml-4">
 			{isClient && (
 				<>
 					<SignedOut>
-						<Button size="lg" onClick={onClick} className="transition-opacity duration-200">
-							Sign in
+						<Button size={buttonSize} onClick={onClick} className="transition-opacity duration-200">
+							{buttonLabel}
 						</Button>
 					</SignedOut>
-				<SignedIn>
-					<div className="relative transition-opacity duration-200">
-						<div className="w-10 h-10 flex items-center justify-center">
-							{/* Agregamos appearance="user-button" para asegurar consistencia en el estilo
-							y afterSignOutUrl para gestionar mejor la redirección después de cerrar sesión*/}
-							<UserButton 
-								appearance={{
-									elements: {
-									userButtonAvatarBox: "h-8 w-8"
-									}
-								}}
-								fallback='/'
-							/>
+					<SignedIn>
+						<div className="relative transition-opacity duration-200">
+							<div className="w-10 h-10 flex items-center justify-center">
+								<UserButton fallback='/' >
+									<UserButton.MenuItems>
+										<UserButton.Link
+											label="My QR Codes"
+											labelIcon={<ScanQrCode size={16} />}
+											href='/qrGenerator'
+										/>
+										<UserButton.Link
+											label="Favorite Themes"
+											labelIcon={<Heart size={16} />}
+											href='/about'
+										/>
+										<UserButton.Action label="manageAccount" />
+									</UserButton.MenuItems>
+								</UserButton>
+							</div>
+							<div className="absolute inset-0 rounded-full pointer-events-none 
+									hover:drop-shadow-[0px_0px_6px_rgba(219,7,61,0.5)] 
+									group-hover:drop-shadow-[0px_0px_10px_rgba(219,7,61,0.7)]
+									transition-all duration-300"
+							></div>
 						</div>
-						{/* Efecto visual separado del componente para mejor rendimiento */}
-						<div className="absolute inset-0 rounded-full pointer-events-none 
-								hover:drop-shadow-[0px_0px_6px_rgba(219,7,61,0.5)] 
-								group-hover:drop-shadow-[0px_0px_10px_rgba(219,7,61,0.7)]
-								transition-all duration-300"
-						></div>
-					</div>
-				</SignedIn>
+					</SignedIn>
 				</>
 			)}
 		</div>
