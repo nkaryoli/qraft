5faca445079308a286d72dba3a832e951f60d5b8
karyoli.ie@gmail.com <148868767+nkaryoli@users.noreply.github.com>
2025-05-10 22:43:38 +0200

    feat: responsive BurgerMenu

--- src/components/header/BurgerMenu.tsx
+++ src/components/header/BurgerMenu.tsx
@@ -1,8 +1,8 @@
 import { memo } from "react";
-import { Sheet, SheetContent, SheetTrigger } from "../ui/sheet";
+import { Sheet, SheetContent, SheetDescription, SheetFooter, SheetTitle, SheetTrigger } from "../ui/sheet";
 import { NavLink } from "react-router-dom";
 import { navigationLinks } from "@/constanst";
-import { TiThMenu } from "react-icons/ti";
+import { Menu } from "lucide-react";
 
 const BurgerMenu = ({ isMobile }:{ isMobile: boolean }) => {
 	if (!isMobile) return null;
@@ -10,16 +10,18 @@ const BurgerMenu = ({ isMobile }:{ isMobile: boolean }) => {
 	return (
 		<Sheet>
 			<SheetTrigger aria-label="Open menu">
-				<TiThMenu className="text-3xl" />
+				<Menu size={35} />
 			</SheetTrigger>
-			<SheetContent>
+			<SheetContent aria-describedby='navegation links'>
+				<SheetTitle className='hidden'>Side Menu</SheetTitle>
+				<SheetDescription className='hidden'></SheetDescription>
 				<ul className="text-center w-full h-full pt-9">
 					{navigationLinks.map(({ to, label }) => (
 						<li key={to}>
 							<NavLink
 								to={to}
 								className={({ isActive }) =>
-									`flex items-center justify-center h-full py-4 transition-colors duration-200
+									`flex items-center justify-center h-full py-4 border-b transition-colors duration-200
 									${isActive ? 'text-primary font-bold bg-accent/15' : 'hover:text-primary-foreground hover:bg-accent/10 '}`
 								}
 							>
@@ -28,6 +30,16 @@ const BurgerMenu = ({ isMobile }:{ isMobile: boolean }) => {
 						</li>
 					))}
 				</ul>
+				<SheetFooter className="relative border-t text-xs flex flex-col items-center justify-center">
+					<div className='absolute w-full h-full burgerFooter'></div>
+					<span>
+						Qraft by 
+						<a href="https://github.com/nkaryoli" className="bg-gradient-to-r from-primary to-secondary bg-clip-text text-transparent ml-1">
+							Karyoli
+						</a>
+					</span>
+					<p className='text-primary'>Hackaton-Clerk-2025</p>
+				</SheetFooter>
 			</SheetContent>
 		</Sheet>
 	)
