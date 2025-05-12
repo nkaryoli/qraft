27ab43e78a45d956da38ff5039a331c228f2b62f
karyoli.ie@gmail.com <148868767+nkaryoli@users.noreply.github.com>
2025-05-12 21:16:56 +0200

    feat: update HomePage

--- src/features/home/HomePage.tsx
+++ src/features/home/HomePage.tsx
@@ -1,116 +1,17 @@
-/* eslint-disable @typescript-eslint/no-explicit-any */
-import Layout from '@/components/Layout';
-import { useSession, useUser } from '@clerk/clerk-react';
-import { createClient } from '@supabase/supabase-js';
-import { useEffect, useState } from 'react';
-import type { SupabaseClient } from '@supabase/supabase-js';
-
-const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
-const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
+import GenerateQRForm from './components/GenerateQRForm';
+import TemplateSection from './components/templateSection/TemplateSection';
 
 const HomePage = () => {
-    const [tasks, setTasks] = useState<any[]>([]);
-    const [loading, setLoading] = useState(true);
-    const [name, setName] = useState('');
-    const { user } = useUser();
-    const { session } = useSession();
-
-    const [client, setClient] = useState<SupabaseClient | null>(null);
-
-    useEffect(() => {
-        const initSupabase = async () => {
-            try{
-                const token = await session?.getToken({ template: 'supabase' });
-                if (!token) {
-                    setLoading(false);
-                    console.log("error fetching tocken");
-                    return;
-                }
-                
-                const supabaseWithAuth = createClient(supabaseUrl, supabaseAnonKey, {
-                    global: {
-                        headers: {
-                            Authorization: `Bearer ${token}`,
-                        },
-                    },
-                });
-                setClient(supabaseWithAuth);
-            } catch (error) {
-                console.error('Error initializing Supabase:', error);
-                setLoading(false);
-            }
-        };
-        
-        if (session) {
-            initSupabase();
-        } else {
-            setLoading(false);
-        }
-    }, [session]);
-
-    useEffect(() => {
-        if (!user || !client) {
-            setLoading(false);
-            return;
-        }
-
-        const loadTasks = async () => {
-            try {
-                setLoading(true);
-                const { data, error } = await client.from('tasks').select();
-
-                if (error) {
-                    console.error('Error loading tasks:', error);
-                    setTasks([]);
-                } else {
-                    setTasks(data || []);
-                }            
-            } catch (error) {
-                console.error('Unexpected error:', error);
-                setTasks([]);
-            } finally {
-                setLoading(false);
-            }
-        };
-
-        loadTasks();
-    }, [user, client]);
-
-    const createTask = async (e: React.FormEvent<HTMLFormElement>) => {
-        e.preventDefault();
-        if (!client) return;
-
-        await client.from('tasks').insert({ name });
-        setName('');
-        // actualiza lista
-        const { data } = await client.from('tasks').select();
-        setTasks(data || []);
-    };
 
     return (
-        <Layout>
-            <div className="p-4 max-w-md mx-auto">
-                <h1 className="text-xl font-bold mb-4">Tasks</h1>
-
-                {loading && <p>Loading...</p>}
-                {!loading && tasks.length > 0 && tasks.map((task) => <p key={task.id}>{task.name}</p>)}
-                {!loading && tasks.length === 0 && <p>No tasks found</p>}
-
-                <form onSubmit={createTask} className="mt-4 flex gap-2">
-                    <input
-                        className="border p-2 flex-grow"
-                        type="text"
-                        name="name"
-                        placeholder="Enter new task"
-                        onChange={(e) => setName(e.target.value)}
-                        value={name}
-                    />
-                    <button type="submit" className="bg-blue-500 text-white px-4 py-2 rounded">
-                        Add
-                    </button>
-                </form>
-            </div>
-        </Layout>
+        <>
+            <section className="flex flex-col items-center justify-center w-full min-h-96 mt-32 px-6 md:px-20">
+                <h1 className='font-extrabold text-5xl text-center mb-6'>Create and Personalize your QR Code</h1>
+                <p className='max-w-lg text-2xl text-center mb-9'>Customize, scan, and share your QR codes in seconds. Effortless and free!!</p>
+                <GenerateQRForm/> 
+            </section>
+            <TemplateSection/>
+        </>
     );
 };
 
