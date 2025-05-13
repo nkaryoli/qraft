855f5afcf6cacf6580c13fbf18eec2650cf33700
karyoli.ie@gmail.com <148868767+nkaryoli@users.noreply.github.com>
2025-05-13 20:35:20 +0200

    feat: add links and icons to burgerMenu

--- src/components/header/BurgerMenu.tsx
+++ src/components/header/BurgerMenu.tsx
@@ -1,34 +1,39 @@
 import { memo } from "react";
-import { Sheet, SheetContent, SheetDescription, SheetFooter, SheetTitle, SheetTrigger } from "../ui/sheet";
-import { NavLink } from "react-router-dom";
+import { Sheet, SheetClose, SheetContent, SheetDescription, SheetFooter, SheetTitle, SheetTrigger } from "../ui/sheet";
 import { navigationLinks } from "@/constanst";
 import { Menu } from "lucide-react";
+import { useLocation } from "react-router-dom";
 
 const BurgerMenu = ({ isMobile }:{ isMobile: boolean }) => {
-	if (!isMobile) return null;
+	const location = useLocation();
 
+	const menuLinks = navigationLinks.map(({ to, label, icon }) => {
+		const isActive = location.pathname === to;
+		const className = `w-full flex items-center gap-3 text-center py-4 px-9 border-b transition-colors duration-200
+		${isActive ? 'text-primary font-bold bg-accent/15' : 'hover:text-primary-foreground hover:bg-accent/10'}`;
+		return (
+			<li key={to}>
+				<SheetClose asChild>
+					<a href={to} className={className}>
+						{icon}
+						{label}
+					</a>
+				</SheetClose>
+			</li>
+		);
+	});
+	
+	if (!isMobile) return null;
 	return (
 		<Sheet>
 			<SheetTrigger aria-label="Open menu">
 				<Menu size={35} />
 			</SheetTrigger>
-			<SheetContent aria-describedby='navegation links'>
+			<SheetContent aria-describedby='navegation links' className="w-[280px] ">
 				<SheetTitle className='hidden'>Side Menu</SheetTitle>
 				<SheetDescription className='hidden'></SheetDescription>
 				<ul className="text-center w-full h-full pt-9">
-					{navigationLinks.map(({ to, label }) => (
-						<li key={to}>
-							<NavLink
-								to={to}
-								className={({ isActive }) =>
-									`flex items-center justify-center h-full py-4 border-b transition-colors duration-200
-									${isActive ? 'text-primary font-bold bg-accent/15' : 'hover:text-primary-foreground hover:bg-accent/10 '}`
-								}
-							>
-								{label}
-							</NavLink>
-						</li>
-					))}
+					{menuLinks}
 				</ul>
 				<SheetFooter className="relative border-t text-xs flex flex-col items-center justify-center">
 					<div className='absolute w-full h-full burgerFooter'></div>
--- src/constanst.tsx
+++ src/constanst.tsx
@@ -1,18 +1,21 @@
-import { ImageUpscale, ScanLine, ScanQrCode } from 'lucide-react';
+import { CircleHelp, House, ImageUpscale, Info, ScanLine, ScanQrCode } from 'lucide-react';
 
 export const navigationLinks = [
-	{ to: '/', label: 'Home' },
-	{ to: '/about', label: 'About' },
-	{ to: '/faq', label: 'faqs' },
+	{ to: '/', label: 'Home', icon: <House size={20} /> },
+	{ to: '/qrGenerator', label: 'Create QR', icon: <ScanQrCode size={20}/>},
+	{ to: '/qrScanner', label: 'QR Reader', icon: <ScanLine size={20} />},
+	{ to: '/dashboard', label: 'Create a Badge', icon: <ImageUpscale size={20} />},
+	{ to: '/about', label: 'About', icon: <Info size={20} /> },
+	{ to: '/faq', label: 'Faqs', icon: <CircleHelp size={20} /> },
 ];
 
 export const desktopNavLinks = [
 	{ to: '/about', label: 'About' },
-	{ to: '/faq', label: 'faqs' },
+	{ to: '/faq', label: 'Faqs' },
 ];
 
 export const serviceLinks = [
 	{ to: '/qrGenerator', label: 'Create QR', icon: <ScanQrCode size={16}/>},
 	{ to: '/qrScanner', label: 'QR Reader', icon: <ScanLine size={16} />},
-	{ to: '/onboarding', label: 'Create a Badge', icon: <ImageUpscale size={16} />},
+	{ to: '/dashboard', label: 'Create a Badge', icon: <ImageUpscale size={16} />},
 ];
