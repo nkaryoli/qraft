57d6c0cffdf0a5620a5574d5cd2f1b13dc79b1e3
karyoli.ie@gmail.com <148868767+nkaryoli@users.noreply.github.com>
2025-05-13 20:36:29 +0200

    fix: header size to avoid jump when openning buegerMenu

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -12,20 +12,22 @@ const Header = () => {
 		navigate('/signin');
 	}
     return (
-        <header className="fixed top-0 flex justify-center h-auto w-full px-6 md:px-9 shadow-lg shabow-bg-100 bg-gradient-to-b from-black to-transparent z-50 backdrop-blur-sm">
-            <nav className="flex justify-between items-center w-full h-16">
-                <NavLink
-                    to="/"
-                    className="flex items-center justify-center bg-gradient-to-r from-primary to-secondary bg-clip-text text-3xl font-extrabold text-transparent"
-                >
-                    Qraft
-                </NavLink>
-                <div className='flex items-center gap-4'>
-                    {isMobile && <SignInBtn isMobile={isMobile} onClick={handleSignin}/>}
-                    <BurgerMenu isMobile={isMobile} />
+        <header className="fixed top-0 left-0 right-0 flex justify-between items-center h-16 w-screen px-6 md:px-9 shadow-lg  bg-gradient-to-b from-black to-transparent z-50 backdrop-blur-sm">
+            <NavLink
+                to="/"
+                className="flex items-center justify-center bg-gradient-to-r from-primary to-secondary bg-clip-text text-3xl font-extrabold text-transparent"
+            >
+                Qraft
+            </NavLink>
+            <div className='flex items-center gap-4'>
+                {isMobile 
+                    ?<>
+                        <SignInBtn isMobile={isMobile} onClick={handleSignin}/>
+                        <BurgerMenu isMobile={isMobile} />
+                    </>
+                    :<Navbar isMobile={isMobile} onClick={handleSignin} />
+                }
                 </div>
-                <Navbar isMobile={isMobile} onClick={handleSignin} />
-            </nav>
         </header>
     );
 };
