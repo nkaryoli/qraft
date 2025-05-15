27d2e54d2915bccc01b375acdbffe8e23c1878c3
karyoli.ie@gmail.com <148868767+nkaryoli@users.noreply.github.com>
2025-05-15 22:42:32 +0200

    feat: add QRPreviewModal for mobile version

--- src/features/custom/components/QRPreview.tsx
+++ src/features/custom/components/QRPreview.tsx
@@ -1,10 +1,11 @@
 import { QRDisplay, type QRDisplayRef } from '@/components/qrCode/QRDisplay';
 import { Button } from '@/components/ui/button';
-import { Card, CardContent, CardHeader, CardTitle } from '@/components/ui/card';
+import { Card, CardContent, CardFooter, CardHeader, CardTitle } from '@/components/ui/card';
 import { useIsMobile } from '@/hooks/useIsMobile';
 import type { QRConfig } from '@/types';
 import { DownloadIcon, HeartPlus } from 'lucide-react';
 import { useRef } from 'react';
+import QRPreviewModal from './QRPreviewModal';
 
 interface QRPreviewProps {
     qrConfig: QRConfig;
@@ -13,39 +14,49 @@ interface QRPreviewProps {
 
 const QRPreview: React.FC<QRPreviewProps> = ({ qrConfig, content }) => {
     const qrRef = useRef<QRDisplayRef>(null);
-    const isMobile = useIsMobile(640);
+    const isMobile = useIsMobile(1040);
 
     const handleDownload = () => {
         qrRef.current?.download('my-qr-code');
     };
 
     return (
-        <Card className="w-[90vw] sm:w-auto">
-            <CardHeader>
-                <CardTitle className='text-foreground text-xl'>Preview</CardTitle>
-            </CardHeader>
-            <CardContent className="space-y-6">
-                <QRDisplay ref={qrRef} config={{ ...qrConfig, data: content }} />
-                <div className="text-sm text-foreground text-center">
-                    <p>Scan your QR Code to test it</p>
-                </div>
-                <div className="flex gap-3 w-full justify-center">
-                    <Button
-                        variant="secondary"
-                        className="gap-2 sm:w-40"
-                        size="lg"
-                        onClick={handleDownload}
-                    >
-                        <DownloadIcon size={4}  />
-                        Download QR
-                    </Button>
-                    <Button className="gap-2 sm:w-40" size="lg">
-                        <HeartPlus size={4} />
-                        {!isMobile && 'Save'}
-                    </Button>
-                </div>
-            </CardContent>
-        </Card>
+        <>
+            {!isMobile ? (
+                <Card className="w-[90vw] sm:w-auto">
+                    <CardHeader>
+                        <CardTitle className='text-foreground text-xl'>Preview</CardTitle>
+                    </CardHeader>
+                    <CardContent className="space-y-6">
+                        <QRDisplay ref={qrRef} config={{ ...qrConfig, data: content }} />
+                        <div className="text-sm text-foreground text-center">
+                            <p>Scan your QR Code to test it</p>
+                        </div>
+                    </CardContent>
+                    <CardFooter className='gap-3 w-full justify-center'>
+                        <Button
+                            variant="secondary"
+                            className="gap-2 sm:w-40"
+                            size="lg"
+                            onClick={handleDownload}
+                        >
+                            <DownloadIcon size={4}  />
+                            Download QR
+                        </Button>
+                        <Button className="gap-2 sm:w-40" size="lg">
+                            <HeartPlus size={4} />
+                            {!isMobile && 'Save'}
+                        </Button>
+                    </CardFooter>
+                </Card>
+            ) : (
+                <QRPreviewModal 
+                    qrRef={qrRef} 
+                    onClick={handleDownload} 
+                    qrConfig={qrConfig} content={content}                    
+                />        
+            ) }
+        </>
     );
 };
 
---
+++ src/features/custom/components/QRPreviewModal.tsx
@@ -0,0 +1,44 @@
+import { QRDisplay, type QRDisplayRef } from "@/components/qrCode/QRDisplay"
+import { Button } from "@/components/ui/button"
+import { Dialog, DialogContent, DialogDescription, DialogFooter, DialogTitle, DialogTrigger } from "@/components/ui/dialog"
+import type { QRConfig } from "@/types";
+import { DownloadIcon, HeartPlus } from "lucide-react"
+
+interface QRPreviewProps {
+	qrRef:  React.RefObject<QRDisplayRef | null>;
+	onClick: () => void;
+	qrConfig: QRConfig;
+	content: string;
+}
+
+const QRPreviewModal:React.FC<QRPreviewProps> = ({qrRef, onClick, qrConfig, content}) => {
+	return (
+		<Dialog>
+			<DialogTrigger className='fixed top-[30vh] -right-20 md:-right-10 -scale-30 md:-scale-50 p-1 border border-withe bg-primary'>
+				<QRDisplay ref={qrRef} config={{ ...qrConfig, data: content }} />
+			</DialogTrigger>
+			<DialogContent className='w-fit p-10 lg:p-20' aria-describedby="qr-preview">
+				<DialogTitle className="hidden"/>
+				<DialogDescription className="hidden"/>
+				<QRDisplay ref={qrRef} config={{ ...qrConfig, data: content }} />
+				<DialogFooter className='sm:flex-row justify-center items-center mt-4'>
+					<Button
+						variant="secondary"
+						className="gap-2 w-full sm:w-40 my-2"
+						size="lg"
+						onClick={onClick}
+					>
+						<DownloadIcon size={4}  />
+						Download QR
+					</Button>
+					<Button className="gap-2 w-full sm:w-40" size="lg">
+						<HeartPlus size={4} />
+						Save
+					</Button>
+				</DialogFooter>
+			</DialogContent>
+		</Dialog>
+	)
+}
+
+export default QRPreviewModal
