bc741122040fcbf57bd8770f23cc08ae8d60baec
karyoli.ie@gmail.com <148868767+nkaryoli@users.noreply.github.com>
2025-05-15 22:24:50 +0200

    feat: add screen sizes to tailwind config

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -3,6 +3,12 @@ import type { Config } from 'tailwindcss';
 const config: Config = {
     content: ['./index.html', './src/**/*.{ts,tsx}'],
     theme: {
+        screens: {
+            sm: '480px',
+            md: '678px',
+            lg: '976px',
+            xl: '1300px',
+        },
         extend: {
             fontFamily: {
                 headerText: ['Sora', 'sans-serif'],
