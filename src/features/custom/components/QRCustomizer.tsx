dfbe34ccdf5492daed95cf991bdd9f249dc86be4
karyoli.ie@gmail.com <148868767+nkaryoli@users.noreply.github.com>
2025-05-15 13:06:17 +0200

    feat: add QRCustomizer component

---
+++ src/features/custom/components/QRCustomizer.tsx
@@ -0,0 +1,54 @@
+import { useState } from 'react';
+import type { QRConfig } from '@/types';
+import { Card, CardContent, CardHeader, CardTitle } from '@/components/ui/card';
+import { Label } from '@/components/ui/label';
+import { Input } from '@/components/ui/input';
+import  QRConfigForm  from './QRConfigForm/QRConfigForm';
+
+interface QRCustomizerProps {
+    qrConfig: QRConfig;
+    onConfigChange: (config: QRConfig) => void;
+    onContentChange: (content: string) => void;
+}
+
+const QRCustomizer: React.FC<QRCustomizerProps> = ({ qrConfig, onContentChange, onConfigChange }) => {
+    const [title, setTitle] = useState('');
+
+	const handleContentChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+        const newContent = e.target.value;
+        onContentChange(newContent);
+    };
+
+    return (
+		<Card className="w-md px-0">
+			<CardHeader>
+				<CardTitle>Customize Options</CardTitle>
+			</CardHeader>
+			<CardContent className="space-y-6">
+				<div className="space-y-2">
+					<Label htmlFor="title">Title</Label>
+					<Input
+						id="title"
+						value={title}
+						onChange={(e) => setTitle(e.target.value)}
+						placeholder="Mi código QR personal"
+					/>
+				</div>
+
+				<div className="space-y-2">
+					<Label htmlFor="content">Content</Label>
+					<Input
+						id="content"
+						value={qrConfig.data}
+						onChange={handleContentChange}
+						placeholder="https://ejemplo.com"
+					/>
+				</div>
+
+				<QRConfigForm config={qrConfig} onChange={onConfigChange} />
+			</CardContent>
+		</Card>
+    );
+};
+
+export default QRCustomizer;
