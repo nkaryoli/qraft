1c544707ebf4b8ed640265de7a2092b902cb6bba
karyoli.ie@gmail.com <148868767+nkaryoli@users.noreply.github.com>
2025-05-16 11:55:12 +0200

    feat: delete not needed file

--- src/features/qrGenerator/components/supabase.ts
+++
@@ -1,5 +0,0 @@
-const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
-const supabaseKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
-import { createClient } from '@supabase/supabase-js';
-
-export const supabase = createClient(supabaseUrl, supabaseKey);
