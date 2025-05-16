8670c44cccbaacc3b07a8a0d87e80667f0dd86e8
karyoli.ie@gmail.com <148868767+nkaryoli@users.noreply.github.com>
2025-05-16 14:16:00 +0200

    feat: add defaultQRConfig variable for cleaner code

--- src/constanst.tsx
+++ src/constanst.tsx
@@ -1,4 +1,5 @@
 import { CircleHelp, House, ImageUpscale, Info, ScanLine, ScanQrCode } from 'lucide-react';
+import type { QRConfig } from './types';
 
 export const navigationLinks = [
     { to: '/', label: 'Home', icon: <House size={20} /> },
@@ -19,3 +20,60 @@ export const serviceLinks = [
     { to: '/qrScanner', label: 'QR Reader', icon: <ScanLine size={16} /> },
     { to: '/dashboard', label: 'Create a Badge', icon: <ImageUpscale size={16} /> },
 ];
+
+export const defaultQRConfig: QRConfig = {
+    width: 250,
+    height: 250,
+    data: '',
+    dotsOptions: {
+        color: '#000000',
+        type: 'square',
+    },
+    dotsOptionsHelper: {
+        colorType: { single: true, gradient: false },
+        gradient: {
+            linear: true,
+            radial: false,
+            color1: '#db073d',
+            color2: '#dba507',
+            rotation: 45,
+        },
+    },
+    backgroundOptions: {
+        color: '#ffffff',
+    },
+    cornersSquareOptions: {
+        type: 'square',
+        color: '#000000',
+    },
+    cornersSquareOptionsHelper: {
+        colorType: { single: true, gradient: false },
+        gradient: {
+            linear: true,
+            radial: false,
+            color1: '#db073d',
+            color2: '#db073d',
+            rotation: 0
+        }
+    },
+    cornersDotOptions: {
+        type: 'square',
+        color: '#000000'
+    },
+    cornersDotOptionsHelper: {
+        colorType: { single: true, gradient: false },
+        gradient: {
+            linear: true,
+            radial: false,
+            color1: '#0d6986',
+            color2: '#0d6986',
+            rotation: 0
+        }
+    },
+    imageOptions: {
+        hideBackgroundDots: true,
+        imageSize: 0.3,
+        margin: 0,
+        saveAsBlob: true
+    },
+};
