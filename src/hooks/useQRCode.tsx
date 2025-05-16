784c625938be585111b5911ed45dc7c62ff39604
karyoli.ie@gmail.com <148868767+nkaryoli@users.noreply.github.com>
2025-05-16 11:51:16 +0200

    feat: add useQRCode hook for handeling conection to qrcode table in supabase

---
+++ src/hooks/useQRCode.tsx
@@ -0,0 +1,35 @@
+import type { QRConfig } from '../types';
+import { useUser } from '@clerk/clerk-react';
+import { useSupabase } from './useSupabaseAuth';
+import { QRCodeAPI } from '@/api/apiQRCode';
+import { useState } from 'react';
+
+export function useQRCode() {
+    const [loading, setLoading] = useState(false);
+    const { user } = useUser();
+    const supabase = useSupabase();
+
+    // Guardar QR en Supabase
+    const handleSaveQRCode = async ( title: string, qr_data: string, qr_template: QRConfig,) => {
+        if (!supabase || !user?.id) return;
+
+        try {
+            setLoading(true);
+
+            const newQR = {
+                qr_data,
+                user_id: user.id,
+                qr_template,
+                title,
+            }
+            const qrApi = QRCodeAPI(supabase);
+            await qrApi.create(newQR);
+        } catch (error) {
+            console.log('Error saving qr:', error);
+        } finally {
+            setLoading(false);
+        }
+    }
+
+    return { loading, handleSaveQRCode };
+}
