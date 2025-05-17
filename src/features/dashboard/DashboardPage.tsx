fe1c3bdc4c4e1f0ea1fa276009afecc8f591c360
karyoli.ie@gmail.com <148868767+nkaryoli@users.noreply.github.com>
2025-05-17 14:51:12 +0200

    feat: add QRTable component for displaying users qrs

--- src/features/dashboard/DashboardPage.tsx
+++ src/features/dashboard/DashboardPage.tsx
@@ -1,8 +1,8 @@
 import { SidebarProvider } from "@/components/ui/sidebar"
 import DashboardSideBar from "./components/DashboardSideBar"
-import Dashboard from "./Dashboard"
 import { useIsMobile } from "@/hooks/useIsMobile";
 import CustomTrigger from "./components/CustomTrigger";
+import QRTable from "./components/QRTable";
 
 const DashboardPage = () => {
 	const isMobile = useIsMobile(975);
@@ -10,7 +10,10 @@ const DashboardPage = () => {
 		<SidebarProvider open>
 			<DashboardSideBar />
 			{isMobile && <CustomTrigger className="left-3" />}
-			<Dashboard />
+			{/* <Dashboard /> */}
+			<section className="w-full pt-32 flex justify-center items-center px-20">
+				<QRTable/>
+			</section>
 		</SidebarProvider>
 	)
 }
