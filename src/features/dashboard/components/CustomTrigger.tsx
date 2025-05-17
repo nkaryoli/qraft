33d5fe49779f6b9810f17b289552f7ff37213aa7
karyoli.ie@gmail.com <148868767+nkaryoli@users.noreply.github.com>
2025-05-17 12:52:09 +0200

    feat: add CustomTriger for sidebar toggle

---
+++ src/features/dashboard/components/CustomTrigger.tsx
@@ -0,0 +1,20 @@
+import { useSidebar } from '@/components/ui/sidebar'
+import { PanelLeftOpen } from 'lucide-react'
+
+interface CustomTriggerProps {
+	className: string;
+}
+const CustomTrigger: React.FC<CustomTriggerProps> = ({className}) => {
+	const { toggleSidebar } = useSidebar()
+
+	return (
+		<button 
+			onClick={toggleSidebar} 
+			className={`fixed top-18 cursor-pointer p-1 border border-transparent transition-colors hover:bg-card hover:border-secondary rounded-md hover:text-secondary z-50 ${className}`} 
+		>
+			<PanelLeftOpen size={26} strokeWidth={1} />
+		</button>
+	)
+}
+
+export default CustomTrigger
