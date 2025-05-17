eb9588a5e618abb542f29aec35b6e60c444606a3
karyoli.ie@gmail.com <148868767+nkaryoli@users.noreply.github.com>
2025-05-17 02:06:21 +0200

    feat: update usQRManager to consume QRContext

--- src/hooks/useQRManager.tsx
+++ src/hooks/useQRManager.tsx
@@ -1,75 +1,23 @@
 import { useUser } from "@clerk/clerk-react";
-import { useLocation, useNavigate } from "react-router-dom";
 import { useSupabase } from "./useSupabaseAuth";
-import { useCallback, useEffect, useRef, useState } from "react";
+import { useCallback, useMemo, useState } from "react";
 import type { QRConfig } from "@/types";
-import { defaultQRConfig } from "@/constanst";
 import { QRCodeAPI } from "@/api/apiQRCode";
-import type { QRDisplayRef } from "@/components/qrCode/QRDisplay";
+import { toast } from "sonner";
+import { useQR } from "./QRContext";
 
 export const useQRManager = () => {
-    const navigate = useNavigate();
-    const location = useLocation();
     const { user } = useUser();
     const supabase = useSupabase();
-    const [isLoading, setIsLoading] = useState(true);
     const [isSaving, setIsSaving] = useState(false);
-    const [title, setTitle] = useState<string>('');
-    const [qrConfig, setQrConfig] = useState<QRConfig>(defaultQRConfig);
-	const qrRef = useRef<QRDisplayRef>(null);
-	
-    const loadQRConfigData = useCallback(() => {
 
-        if (location.state?.qrConfig) {
-            setQrConfig(prevConfig => ({
-                ...prevConfig,
-                ...location.state.qrConfig
-            }));
-            setIsLoading(false);
-            return;
-        }
+    const { qrRef, qrConfig } = useQR();
 
-        const pendingQRConfig = sessionStorage.getItem('pendingQRConfig');
-
-        if (pendingQRConfig) {
-            try {
-                const parsedData = JSON.parse(pendingQRConfig);
-                const configData = parsedData.config || parsedData;
-                
-                if (configData) {
-                    setQrConfig(prevConfig => ({
-                        ...prevConfig,
-                        ...configData,
-                        data: configData.data || ''
-                    }));
-                }
-            } catch (error) {
-                console.error('Error parsing QR config from sessionStorage:', error);
-            }
-        } else if (!location.state?.qrConfig) {
-            navigate('/');
-            return;
-        }
-        
-        setIsLoading(false);
-    }, [location, navigate]);
-
-    useEffect(() => {
-        loadQRConfigData();
-    }, [loadQRConfigData]);
-
-    const handleContentChange = (content: string) => {
-        setQrConfig((prev) => ({
-            ...prev,
-            data: content,
-        }));
-    };
-
-	const handleDownload = () => {
+	const handleDownload = useCallback(() => {
         qrRef.current?.download('my-qr-code');
-    };
+    }, [qrRef]);
 
-    const handleSaveQRCode = async (title: string, qr_data: string = qrConfig.data, qr_template: QRConfig = qrConfig) => {
+    const handleSaveQRCode = useCallback(async (title: string, qr_data: string = qrConfig.data, qr_template: QRConfig = qrConfig) => {
         if (!supabase || !user?.id) return;
 
         try {
@@ -82,34 +30,28 @@ export const useQRManager = () => {
                 title,
             }
             const qrApi = QRCodeAPI(supabase);
+            console.log('Saving QR:', newQR);
             await qrApi.create(newQR);
             
             if (sessionStorage.getItem('pendingQRConfig')) {
                 sessionStorage.removeItem('pendingQRConfig');
             }
-            
-            return true;
+            toast("Nice! QR saved 🎉", {
+                description: "Check it out in your list of saved codes.",
+                duration: 4000,
+                style: {backgroundColor: '#eaeaea', color:'#07485b'}
+            })
+            // return true;
         } catch (error) {
             console.error('Error saving QR:', error);
             return false;
         } finally {
             setIsSaving(false);
         }
-    };
+    }, [supabase, user?.id, qrConfig]);
 
-    return {
-        // Estado
-        isLoading,
-        isSaving,
-        title,
-        setTitle,
-        qrConfig,
-        setQrConfig,
-        qrRef,
-
-        // Métodos
-		handleDownload,
-        handleContentChange,
-        handleSaveQRCode
-    };
+    return useMemo(() => (
+        { isSaving, handleDownload, handleSaveQRCode}), 
+        [ isSaving, handleDownload, handleSaveQRCode ]
+    );
 };
