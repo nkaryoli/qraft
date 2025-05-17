44e25c53e087e8b808c744189841971b8315ebca
karyoli.ie@gmail.com <148868767+nkaryoli@users.noreply.github.com>
2025-05-17 02:09:22 +0200

    feat: refactor props of QRPreviewModal

--- src/features/custom/components/QRPreviewModal.tsx
+++ src/features/custom/components/QRPreviewModal.tsx
@@ -1,4 +1,4 @@
-import { QRDisplay, type QRDisplayRef } from "@/components/qrCode/QRDisplay"
+import QRDisplay, { type QRDisplayRef } from "@/components/qrCode/QRDisplay"
 import { Button } from "@/components/ui/button"
 import { Dialog, DialogContent, DialogDescription, DialogFooter, DialogTitle, DialogTrigger } from "@/components/ui/dialog"
 import type { QRConfig } from "@/types";
@@ -6,22 +6,27 @@ import { DownloadIcon, HeartPlus } from "lucide-react"
 
 interface QRPreviewProps {
 	qrRef:  React.RefObject<QRDisplayRef | null>;
-	onSave: () => void;
+	onSave: (title: string, qrData: string, config: QRConfig) => void;
 	onDownload: () => void;
 	qrConfig: QRConfig;
-	content: string;
+	title: string;
 }
 
-const QRPreviewModal:React.FC<QRPreviewProps> = ({qrRef, onSave, onDownload, qrConfig, content}) => {
+const QRPreviewModal:React.FC<QRPreviewProps> = ({qrRef, onSave, onDownload, qrConfig, title}) => {
+	
+	const handleSave = () => {
+        onSave(title, qrConfig.data, qrConfig);
+    };
+
 	return (
 		<Dialog>
 			<DialogTrigger className='fixed top-[30vh] -right-20 md:-right-10 -scale-30 md:-scale-50 p-1 border border-withe bg-primary'>
-				<QRDisplay ref={qrRef} config={{ ...qrConfig, data: content }} />
+				<QRDisplay ref={qrRef} config={qrConfig} />
 			</DialogTrigger>
 			<DialogContent className='w-fit p-10 lg:p-20' aria-describedby="qr-preview">
 				<DialogTitle className="hidden"/>
 				<DialogDescription className="hidden"/>
-				<QRDisplay ref={qrRef} config={{ ...qrConfig, data: content }} />
+				<QRDisplay ref={qrRef} config={qrConfig} />
 				<DialogFooter className='sm:flex-row justify-center items-center mt-4'>
 					<Button
 						variant="secondary"
@@ -34,7 +39,7 @@ const QRPreviewModal:React.FC<QRPreviewProps> = ({qrRef, onSave, onDownload, qrC
 					</Button>
 					<Button 
 						className="gap-2 w-full sm:w-40" size="lg" 						
-						onClick={onSave}
+						onClick={handleSave}
 					>
 						<HeartPlus size={4} />
 						Save
