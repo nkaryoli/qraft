bf1b2c126f6548247fb61c1e11c21849ab2d5dfa
karyoli.ie@gmail.com <148868767+nkaryoli@users.noreply.github.com>
2025-05-17 12:54:12 +0200

    feat: update z index of header

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -12,7 +12,7 @@ const Header = () => {
         navigate('/signin');
     };
     return (
-        <header className="fixed top-0 left-0 right-0 flex justify-between items-center h-16 w-screen px-6 md:px-9 shadow-lg  bg-gradient-to-b from-black to-transparent z-50 backdrop-blur-sm">
+        <header className="fixed top-0 left-0 right-0 flex justify-between items-center h-16 w-screen px-6 md:px-9 shadow-lg  bg-gradient-to-b from-black to-transparent z-[99999999] backdrop-blur-sm">
             <NavLink
                 to="/"
                 className="flex items-center justify-center bg-gradient-to-r from-primary to-secondary bg-clip-text text-3xl font-extrabold text-transparent"
