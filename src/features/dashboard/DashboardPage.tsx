ef95862036d32942770d7708f877f1e2d206026c
karyoli.ie@gmail.com <148868767+nkaryoli@users.noreply.github.com>
2025-05-18 12:14:14 +0200

    feat: implent correct component to be shown for sidebar component

--- src/features/dashboard/DashboardPage.tsx
+++ src/features/dashboard/DashboardPage.tsx
@@ -3,17 +3,22 @@ import DashboardSideBar from "./components/DashboardSideBar"
 import { useIsMobile } from "@/hooks/useIsMobile";
 import CustomTrigger from "./components/CustomTrigger";
 import QRTable from "./components/QRTable";
+import { useState } from "react";
+
+const sections = {
+	"saved-qr":     <QRTable/>,
+	"my-templates": <h1>my templates</h1>,
+	"my-orgs":      <h1>my orpanizations</h1>,
+};
 
 const DashboardPage = () => {
+	const [active, setActive] = useState<keyof typeof sections>("saved-qr");
 	const isMobile = useIsMobile(975);
 	return (
 		<SidebarProvider open>
-			<DashboardSideBar />
+			<DashboardSideBar onSelect={setActive} active={active}/>
 			{isMobile && <CustomTrigger className="left-3" />}
-			{/* <Dashboard /> */}
-			<section className="w-full pt-32 flex justify-center items-center px-20">
-				<QRTable/>
-			</section>
+			<section className="w-full pt-32 flex justify-center items-center px-20">{sections[active]}</section>
 		</SidebarProvider>
 	)
 }
