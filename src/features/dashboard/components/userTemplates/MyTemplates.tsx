efd4526aa21c15793fafc0cf93e361af840b800d
karyoli.ie@gmail.com <148868767+nkaryoli@users.noreply.github.com>
2025-05-18 20:28:56 +0200

    feat: add MyTemplate section

---
+++ src/features/dashboard/components/userTemplates/MyTemplates.tsx
@@ -0,0 +1,25 @@
+import TemplateCard from "./TemplateCard"
+import type { QRCode } from "@/supabase/types";
+
+interface MyTemplatesProps {
+	qrs: QRCode[];
+}
+
+const MyTemplates:React.FC<MyTemplatesProps> = ({ qrs }) => {
+
+	return (
+		<div className="space-y-6">
+			<h2 className="text-2xl font-semibold">My Templates</h2>
+			<div className="grid grid-cols-1 lg:grid-cols-2 xl:grid-cols-3 gap-6">
+				{qrs.map((qr) => (
+					<TemplateCard 
+						key={qr.id} 
+						qrTemplate={qr.qr_template}
+					/>
+				))}
+			</div>
+		</div>
+	)
+}
+
+export default MyTemplates
