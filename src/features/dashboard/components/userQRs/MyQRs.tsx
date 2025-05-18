ffa009b3e3ba1fa4f26c088be7bcaca3ce2eda61
karyoli.ie@gmail.com <148868767+nkaryoli@users.noreply.github.com>
2025-05-18 20:25:59 +0200

    feat: add MyQRS section

---
+++ src/features/dashboard/components/userQRs/MyQRs.tsx
@@ -0,0 +1,17 @@
+import type { QRCode } from "@/supabase/types";
+import QRTable from "./QRTable"
+
+interface MyQRsProps {
+	qrs: QRCode[];
+}
+
+const MyQRs:React.FC<MyQRsProps> = ({qrs}) => {
+	return (
+		<div className="w-full flex flex-col items-center justify-center gap-6">
+			<h2 className="text-2xl font-semibold w-full max-w-5xl">My QR Codes</h2>
+			<QRTable qrs={qrs}/>
+		</div>
+	)
+}
+
+export default MyQRs
