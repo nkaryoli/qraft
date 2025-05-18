56bdd54225ba5ea0704af7bc8ad404fc66a29fdf
karyoli.ie@gmail.com <148868767+nkaryoli@users.noreply.github.com>
2025-05-18 12:16:08 +0200

    feat: add modal for showing qr at QRTable

--- src/features/dashboard/components/QRTable.tsx
+++ src/features/dashboard/components/QRTable.tsx
@@ -1,16 +1,18 @@
-import QRDisplay from "@/components/qrCode/QRDisplay";
-import { Button } from "@/components/ui/button";
-import { Table, TableBody, TableCaption, TableCell, TableHead, TableHeader, TableRow } from "@/components/ui/table"
-import { useQR } from "@/hooks/QRContext";
+import { type QRDisplayRef } from "@/components/qrCode/QRDisplay";
+import ShowDetailsModal from "@/features/dashboard/components/ShowDetailsModal";
+import { Table, TableBody, TableCell, TableHead, TableHeader, TableRow } from "@/components/ui/table"
 import { useQRManager } from "@/hooks/useQRManager"
 import type { QRCode } from "@/supabase/types";
-import { useEffect, useState } from "react";
+import { useEffect, useRef, useState } from "react";
+import { Button } from "@/components/ui/button";
 
 
 const QRTable = () => {
 	const { loadQRs } = useQRManager();
 	const [ qrs , setQrs ] = useState<QRCode[]>([]);
-	const { qrRef } = useQR();
+	const qrRef = useRef<QRDisplayRef>(null);
+	const { handleDownload } = useQRManager();
+
 	useEffect(() => {
 		const fetchQRs = async () => {
 			const qrs = await loadQRs();
@@ -22,33 +24,30 @@ const QRTable = () => {
 
 	return (
 		<div className="w-full max-w-4xl bg-background rounded-lg">
-		<Table className="bg-gradient-to-bl form-background to-muted max-w-4xl rounded-lg px-6">
-			<TableCaption>A list of your recent invoices.</TableCaption>
-			<TableHeader className="bg-muted rounded-t-lg">
-				<TableRow>
-					<TableHead className="w-[100px] rounded-tl-lg ">Title</TableHead>
-					<TableHead>Content</TableHead>
-					<TableHead>Creation Date</TableHead>
-					<TableHead className="rounded-tr-lg">QR Code</TableHead>
-				</TableRow>
-			</TableHeader>
-			<TableBody>
-				{qrs.map((qr) => (
-					<TableRow key={qr.id}>
-						<TableCell className="font-medium">{qr.title}</TableCell>
-						<TableCell>{qr.qr_data}</TableCell>
-						<TableCell>TODO</TableCell>
-						<TableCell>
-							<Button variant='outline' size='sm'>view QR</Button>
-							<QRDisplay ref={qrRef} config={qr.qr_template} />
-						</TableCell>
+			<Table className="bg-gradient-to-bl form-background to-muted max-w-4xl rounded-lg px-6">
+				<TableHeader className="bg-muted rounded-t-lg">
+					<TableRow>
+						<TableHead className="w-[100px] rounded-tl-lg ">Title</TableHead>
+						<TableHead>Content</TableHead>
+						<TableHead>Creation Date</TableHead>
+						<TableHead className="rounded-tr-lg">QR Code</TableHead>
 					</TableRow>
-				))}
-			</TableBody>
-		</Table>
-		<div>
-			
-		</div>
+				</TableHeader>
+				<TableBody>
+					{qrs.map((qr) => (
+						<TableRow key={qr.id}>
+							<TableCell className="font-medium">{qr.title}</TableCell>
+							<TableCell>{qr.qr_data}</TableCell>
+							<TableCell>TODO</TableCell>
+							<TableCell>
+								<ShowDetailsModal qrRef={qrRef} onDownload={handleDownload} qrConfig={qr.qr_template} >
+									<Button variant='outline' size='sm'>view QR</Button>
+								</ShowDetailsModal> 
+							</TableCell>
+						</TableRow>
+					))}
+				</TableBody>
+			</Table>
 		</div>
 	)
 }
