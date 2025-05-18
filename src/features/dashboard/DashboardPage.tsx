446e8921fa00d954ef9fac106a49c8b515a8b801
karyoli.ie@gmail.com <148868767+nkaryoli@users.noreply.github.com>
2025-05-18 20:29:21 +0200

    feat: add loader

--- src/features/dashboard/DashboardPage.tsx
+++ src/features/dashboard/DashboardPage.tsx
@@ -2,23 +2,63 @@ import { SidebarProvider } from "@/components/ui/sidebar"
 import DashboardSideBar from "./components/DashboardSideBar"
 import { useIsMobile } from "@/hooks/useIsMobile";
 import CustomTrigger from "./components/CustomTrigger";
-import QRTable from "./components/QRTable";
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import MyTemplates from "./components/userTemplates/MyTemplates";
+import MyQRs from "./components/userQRs/MyQRs";
+import type { QRCode } from "@/supabase/types";
+import { useQRManager } from "@/hooks/useQRManager";
+import { BarLoader, PacmanLoader } from "react-spinners";
 
 const sections = {
-	"saved-qr":     <QRTable/>,
-	"my-templates": <h1>my templates</h1>,
-	"my-orgs":      <h1>my orpanizations</h1>,
+	"saved-qr": MyQRs,
+	"my-templates": MyTemplates,
+	"my-orgs": () => <h1>my organizations</h1>,
 };
 
 const DashboardPage = () => {
 	const [active, setActive] = useState<keyof typeof sections>("saved-qr");
 	const isMobile = useIsMobile(975);
+	const [qrs, setQrs] = useState<QRCode[]>([]);
+	const [isLoading, setIsLoading] = useState(true);
+	const { loadQRs } = useQRManager();
+
+	useEffect(() => {
+		const fetchQRs = async () => {
+			setIsLoading(true);
+			try {
+				const data = await loadQRs();
+				setQrs(data);
+			} catch (error) {
+				console.error("Error loading QRs:", error);
+				setQrs([]); 
+			} finally {
+				setIsLoading(false);
+			}
+		};
+
+		fetchQRs();
+	}, [loadQRs]);
+
+	const ActiveComponent = sections[active];
+
 	return (
 		<SidebarProvider open>
 			<DashboardSideBar onSelect={setActive} active={active}/>
 			{isMobile && <CustomTrigger className="left-3" />}
-			<section className="w-full pt-32 flex justify-center items-center px-20">{sections[active]}</section>
+			<section className="w-full flex justify-center py-20 px-6">
+				{isLoading && (
+					<div className="fixed top-16 left-0 w-full z-50 pointer-events-none">
+						<BarLoader width="100%" color="#db073d" speedMultiplier={0.5} />
+					</div>
+				)}
+				{qrs?.length === 0 ? (
+					<div className="w-full h-full flex flex-col justify-center max-w-5xl space-y-4">
+						<PacmanLoader  size={50} color="#db073d" className="absolute left-[40%]" speedMultiplier={0.5}  />
+					</div>
+				) : (
+					<ActiveComponent qrs={qrs} />
+				)}
+			</section>
 		</SidebarProvider>
 	)
 }
