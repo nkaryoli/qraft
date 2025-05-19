9e7eac3b4c30824a82c5b5ad2595216c4e2fffe2
karyoli.ie@gmail.com <148868767+nkaryoli@users.noreply.github.com>
2025-05-19 09:05:31 +0200

    feat: update navbar links

--- src/constanst.tsx
+++ src/constanst.tsx
@@ -1,32 +1,29 @@
-import { CircleHelp, House, ImageUpscale, Info, ScanLine, ScanQrCode } from 'lucide-react';
+import { CircleHelp, House, Info, ScanLine, ScanQrCode } from 'lucide-react';
 import type { QRConfig } from './types';
 
 export const navigationLinks = [
     { to: '/', label: 'Home', icon: <House size={20} /> },
-    { to: '/qrGenerator', label: 'Create QR', icon: <ScanQrCode size={20} /> },
+    { to: '/Customizer', label: 'Create QR', icon: <ScanQrCode size={20} /> },
     { to: '/qrScanner', label: 'QR Reader', icon: <ScanLine size={20} /> },
-    { to: '/dashboard', label: 'Create a Badge', icon: <ImageUpscale size={20} /> },
     { to: '/about', label: 'About', icon: <Info size={20} /> },
     { to: '/faq', label: 'Faqs', icon: <CircleHelp size={20} /> },
 ];
 
 export const desktopNavLinks = [
+    { to: '/', label: 'Home' },
+    { to: '/customize', label: 'Create QR' },
+    { to: '/qrScanner', label: 'QR Reader' },
     { to: '/about', label: 'About' },
     { to: '/faq', label: 'Faqs' },
 ];
 
-export const serviceLinks = [
-    { to: '/qrGenerator', label: 'Create QR', icon: <ScanQrCode size={16} /> },
-    { to: '/qrScanner', label: 'QR Reader', icon: <ScanLine size={16} /> },
-    { to: '/dashboard', label: 'Create a Badge', icon: <ImageUpscale size={16} /> },
-];
 export const defaultQRConfig: QRConfig = {
     width: 250,
     height: 250,
     data: '',
-    type: 'svg', // Añadido para coincidir con DrawType
-    margin: 10, // Valor por defecto recomendado
-    qrOptions: { // Añadido para coincidir con la librería
+    type: 'svg',
+    margin: 10,
+    qrOptions: {
         typeNumber: 0,
         mode: 'Byte',
         errorCorrectionLevel: 'Q'
@@ -46,7 +43,6 @@ export const defaultQRConfig: QRConfig = {
         type: 'square',
         color: '#000000'
     },
-    // Helpers (específicos de tu UI)
     dotsOptionsHelper: {
         colorType: { single: true, gradient: false },
         gradient: {
